b28a443c0177f633f998ab6f4d9fb78a24410888
Luisa Helena <luisa.lelis@sga.pucminas.br>
2022-07-31 19:05:00 +0100

    Estilizacao da home

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from 'components/Navbar';
+import Home from 'pages/Home';
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <h1>Helo DsCatalog</h1>
+      <Home />
     </>
   );
 };
