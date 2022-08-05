0c4dbe08800b6a990ce89541de4e42f6c8783ad5
Luisa Helena <luisa.lelis@sga.pucminas.br>
2022-08-05 12:00:00 +0100

    Props ProductCard

--- frontweb/src/components/ProductCard/index.tsx
+++ frontweb/src/components/ProductCard/index.tsx
@@ -1,22 +1,24 @@
 import './styles.css';
 
-import ProductImg from 'assets/images/product.png';
 import ProductPrice from 'components/ProductPrice';
+import { Product } from 'types/product';
 
+type Props = {
+  product: Product;
+};
 
-const ProductCard = () => {
-
-    return (
-        <div className="base-card product-card">
-            <div className="card-top-container">
-                <img src={ProductImg} alt="Nome do produto" />
-            </div>
-            <div className="card-botton-container">
-                <h6>Computador Desktop - Intel Core i7</h6>
-                <ProductPrice />
-            </div>
-        </div>
-    );
+const ProductCard = ({ product }: Props) => {
+  return (
+    <div className="base-card product-card">
+      <div className="card-top-container">
+        <img src={product.imgUrl} alt={product.name} />
+      </div>
+      <div className="card-botton-container">
+        <h6>{product.name}</h6>
+        <ProductPrice price={product.price} />
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
